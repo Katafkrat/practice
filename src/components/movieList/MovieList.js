858a938ddb21852dd1fcb13427c8dcc28e7ece0f
Katafkrat <Ivan20040208@gmail.com>
2024-07-03 21:59:03 +0300

    MovieList - was added No-Image-Placeholder

--- src/components/movieList/MovieList.js
+++ src/components/movieList/MovieList.js
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
+import noImagePlaceholder from '../../resource/No-Image-Placeholder.png'
+
 import './MovieList.scss'
 
 const MovieList = ({ title, data = [] }) => {
@@ -12,7 +14,9 @@ const MovieList = ({ title, data = [] }) => {
 				{data.slice(0, 12).map(movie => (
 					<Link to={`/movie/${movie?.id}`} key={movie?.id}>
 						<li className='card__item'>
-							<img src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`} alt={movie.title} />
+							<img src={movie?.poster_path
+								? `https://image.tmdb.org/t/p/w500${movie?.poster_path}`
+								: noImagePlaceholder} alt={movie?.title} />
 							<div className="card__title">{movie?.title}</div>
 						</li>
 					</Link>
